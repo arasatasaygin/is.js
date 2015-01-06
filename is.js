9b2c917dbf515fd98a944a7ccb751734acda82ff
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-01-07 00:10:07 +0200

    Change parameters to indicate type.

--- is.js
+++ is.js
@@ -259,30 +259,30 @@
     // String checks
     /* -------------------------------------------------------------------------- */
     
-    // is a given value all uppercase?
-    is.upperCase = function(value) {
-        return is.string(value) && value === value.toUpperCase();
+    // is a given string all uppercase?
+    is.upperCase = function(str) {
+        return is.string(str) && str === str.toUpperCase();
     };
 
-    // is a given value all lowercase?
-    is.lowerCase = function(value) {
-        return is.string(value) && value === value.toLowerCase();
+    // is a given string all lowercase?
+    is.lowerCase = function(str) {
+        return is.string(str) && str === str.toLowerCase();
     };
 
-    // is value start with a given startWith parameter?
-    is.startWith = function(value, startWith) {
-        return is.string(value) && value.indexOf(startWith) === 0;
+    // is string start with a given startWith parameter?
+    is.startWith = function(str, startWith) {
+        return is.string(str) && str.indexOf(startWith) === 0;
     };
 
-    // is value end with a given endWith parameter?
-    is.endWith = function(value, endWith) {
-        return is.string(value) && value.indexOf(endWith) === value.length -  endWith.length;
+    // is string end with a given endWith parameter?
+    is.endWith = function(str, endWith) {
+        return is.string(str) && str.indexOf(endWith) === str.length -  endWith.length;
     };
 
-    // is a given string value or sentence capitalized?
-    is.capitalized = function(value) {
-        if(is.not.string(value)) return false;
-        var words = value.split(' ');
+    // is a given string string or sentence capitalized?
+    is.capitalized = function(str) {
+        if(is.not.string(str)) return false;
+        var words = str.split(' ');
         var capitalized = [];
         for(var i = 0; i < words.length; i++) {
             capitalized.push(words[i][0] === words[i][0].toUpperCase());
@@ -290,12 +290,12 @@
         return is.all.truthy.apply(null, capitalized);
     };
 
-    // is a given value safe word?
+    // is a given string safe word?
     // the dirty words from Google's "what do you love" project - https://gist.github.com/jamiew/1112488
-    is.safeWord = function(value) {
+    is.safeWord = function(str) {
         var badWords = ["4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lmfao", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"];
         for(var i = 0; i < badWords.length; i++){
-            if(badWords[i] === value) return false;
+            if(badWords[i] === str) return false;
         }
         return true;
     };
@@ -303,34 +303,34 @@
     // Time checks
     /* -------------------------------------------------------------------------- */
     
-    // is a given date object indicate today?
-    is.today = function(value) {
+    // is a given date indicate today?
+    is.today = function(obj) {
         var now = new Date();
         var todayString = now.toDateString();
-        return is.date(value) && value.toDateString() === todayString;
+        return is.date(obj) && obj.toDateString() === todayString;
     };
 
-    // is a given date object indicate yesterday?
-    is.yesterday = function(value) {
+    // is a given date indicate yesterday?
+    is.yesterday = function(obj) {
         var now = new Date();
         var yesterdayString = new Date(now.setDate(now.getDate() - 1)).toDateString();
-        return is.date(value) && value.toDateString() === yesterdayString;
+        return is.date(obj) && obj.toDateString() === yesterdayString;
     };
 
-    // is a given date object indicate tomorrow?
-    is.tomorrow = function(value) {
+    // is a given date indicate tomorrow?
+    is.tomorrow = function(obj) {
         var now = new Date();
         var tomorrowString = new Date(now.setDate(now.getDate() + 1)).toDateString();
-        return is.date(value) && value.toDateString() === tomorrowString;
+        return is.date(obj) && obj.toDateString() === tomorrowString;
     };
 
-    // is a given date object past?
-    is.past = function(value) {
+    // is a given date past?
+    is.past = function(obj) {
         var now = new Date();
-        return is.date(value) && value.getTime() < now.getTime();
+        return is.date(obj) && obj.getTime() < now.getTime();
     };
 
-    // is a given date object future?
+    // is a given date future?
     is.future = not(is.past);
 
     // Environment checks
