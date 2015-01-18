52d7a1572c601b441f675a2945fb3e89aa91f640
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-01-18 23:14:52 +0200

    Check if library is used as a Node.js module.

--- is.js
+++ is.js
@@ -487,150 +487,155 @@
 
     // Environment checks
     /* -------------------------------------------------------------------------- */
+    
+    // check if library is used as a Node.js module
+    if(typeof window !== 'undefined') {
 
-    var userAgent = 'navigator' in window && navigator.userAgent.toLowerCase() || '';
-    var vendor = 'navigator' in window && navigator.vendor.toLowerCase() || '';
-    var appVersion = 'navigator' in window && navigator.appVersion.toLowerCase() || '';
+        // store navigator properties to use later
+        var userAgent = 'navigator' in window && navigator.userAgent.toLowerCase() || '';
+        var vendor = 'navigator' in window && navigator.vendor.toLowerCase() || '';
+        var appVersion = 'navigator' in window && navigator.appVersion.toLowerCase() || '';
 
-    // is current browser chrome?
-    is.chrome = function() {
-        return /chrome|chromium/i.test(userAgent) && /google inc/.test(vendor);
-    };
-    // chrome method does not support 'all' and 'any' interfaces
-    is.chrome.api = ['not'];
+        // is current browser chrome?
+        is.chrome = function() {
+            return /chrome|chromium/i.test(userAgent) && /google inc/.test(vendor);
+        };
+        // chrome method does not support 'all' and 'any' interfaces
+        is.chrome.api = ['not'];
 
-    // is current browser firefox?
-    is.firefox = function() {
-        return /firefox/i.test(userAgent);
-    };
-    // firefox method does not support 'all' and 'any' interfaces
-    is.firefox.api = ['not'];
+        // is current browser firefox?
+        is.firefox = function() {
+            return /firefox/i.test(userAgent);
+        };
+        // firefox method does not support 'all' and 'any' interfaces
+        is.firefox.api = ['not'];
 
-    // is current browser internet explorer?
-    // parameter is optional
-    is.ie = function(version) {
-        if(!version) return /msie/i.test(userAgent);
-        return new RegExp('msie ' + version).test(userAgent);
-    };
-    // ie method does not support 'all' and 'any' interfaces
-    is.ie.api = ['not'];
+        // is current browser internet explorer?
+        // parameter is optional
+        is.ie = function(version) {
+            if(!version) return /msie/i.test(userAgent);
+            return new RegExp('msie ' + version).test(userAgent);
+        };
+        // ie method does not support 'all' and 'any' interfaces
+        is.ie.api = ['not'];
 
-    // is current browser opera?
-    is.opera = function() {
-        return /opr/i.test(userAgent);
-    };
-    // opera method does not support 'all' and 'any' interfaces
-    is.opera.api = ['not'];
+        // is current browser opera?
+        is.opera = function() {
+            return /opr/i.test(userAgent);
+        };
+        // opera method does not support 'all' and 'any' interfaces
+        is.opera.api = ['not'];
 
-    // is current browser safari?
-    is.safari = function() {
-        return /safari/i.test(userAgent) && /apple computer/i.test(vendor);
-    };
-    // safari method does not support 'all' and 'any' interfaces
-    is.safari.api = ['not'];
+        // is current browser safari?
+        is.safari = function() {
+            return /safari/i.test(userAgent) && /apple computer/i.test(vendor);
+        };
+        // safari method does not support 'all' and 'any' interfaces
+        is.safari.api = ['not'];
 
-    // is current device ios?
-    is.ios = function() {
-        return is.iphone() || is.pad() || is.ipod();
-    };
-    // ios method does not support 'all' and 'any' interfaces
-    is.ios.api = ['not'];
+        // is current device ios?
+        is.ios = function() {
+            return is.iphone() || is.pad() || is.ipod();
+        };
+        // ios method does not support 'all' and 'any' interfaces
+        is.ios.api = ['not'];
 
-    // is current device iphone?
-    is.iphone = function() {
-        return /iphone/i.test(userAgent);
-    };
-    // iphone method does not support 'all' and 'any' interfaces
-    is.iphone.api = ['not'];
+        // is current device iphone?
+        is.iphone = function() {
+            return /iphone/i.test(userAgent);
+        };
+        // iphone method does not support 'all' and 'any' interfaces
+        is.iphone.api = ['not'];
 
-    // is current device ipad?
-    is.ipad = function() {
-        return /ipad/i.test(userAgent);
-    };
-    // ipad method does not support 'all' and 'any' interfaces
-    is.ipad.api = ['not'];
+        // is current device ipad?
+        is.ipad = function() {
+            return /ipad/i.test(userAgent);
+        };
+        // ipad method does not support 'all' and 'any' interfaces
+        is.ipad.api = ['not'];
 
-    // is current device ipod?
-    is.ipod = function() {
-        return /ipod/i.test(userAgent);
-    };
-    // ipod method does not support 'all' and 'any' interfaces
-    is.ipod.api = ['not'];
+        // is current device ipod?
+        is.ipod = function() {
+            return /ipod/i.test(userAgent);
+        };
+        // ipod method does not support 'all' and 'any' interfaces
+        is.ipod.api = ['not'];
 
-    // is current device android?
-    is.android = function() {
-        return /android/i.test(userAgent);
-    };
-    // android method does not support 'all' and 'any' interfaces
-    is.android.api = ['not'];
+        // is current device android?
+        is.android = function() {
+            return /android/i.test(userAgent);
+        };
+        // android method does not support 'all' and 'any' interfaces
+        is.android.api = ['not'];
 
-    // is current device android tablet?
-    is.androidPhone = function() {
-        return /android/i.test(userAgent) && /mobile/i.test(userAgent);
-    };
-    // androidPhone method does not support 'all' and 'any' interfaces
-    is.androidPhone.api = ['not'];
+        // is current device android tablet?
+        is.androidPhone = function() {
+            return /android/i.test(userAgent) && /mobile/i.test(userAgent);
+        };
+        // androidPhone method does not support 'all' and 'any' interfaces
+        is.androidPhone.api = ['not'];
 
-    // is current device android tablet?
-    is.androidTablet = function() {
-        return /android/i.test(userAgent) && !/mobile/i.test(userAgent);
-    };
-    // androidTablet method does not support 'all' and 'any' interfaces
-    is.androidTablet.api = ['not'];
+        // is current device android tablet?
+        is.androidTablet = function() {
+            return /android/i.test(userAgent) && !/mobile/i.test(userAgent);
+        };
+        // androidTablet method does not support 'all' and 'any' interfaces
+        is.androidTablet.api = ['not'];
 
-    // is current device mobile?
-    is.mobile = function() {
-        return is.iphone() || is.ipod() || is.androidPhone();
-    };
-    // mobile method does not support 'all' and 'any' interfaces
-    is.mobile.api = ['not'];
+        // is current device mobile?
+        is.mobile = function() {
+            return is.iphone() || is.ipod() || is.androidPhone();
+        };
+        // mobile method does not support 'all' and 'any' interfaces
+        is.mobile.api = ['not'];
 
-    // is current device tablet?
-    is.tablet = function() {
-        return is.ipad() || is.androidTablet();
-    };
-    // tablet method does not support 'all' and 'any' interfaces
-    is.tablet.api = ['not'];
+        // is current device tablet?
+        is.tablet = function() {
+            return is.ipad() || is.androidTablet();
+        };
+        // tablet method does not support 'all' and 'any' interfaces
+        is.tablet.api = ['not'];
 
-    // is current device desktop?
-    is.desktop = function() {
-        return is.not.mobile() && is.not.tablet();
-    };
-    // desktop method does not support 'all' and 'any' interfaces
-    is.desktop.api = ['not'];
+        // is current device desktop?
+        is.desktop = function() {
+            return is.not.mobile() && is.not.tablet();
+        };
+        // desktop method does not support 'all' and 'any' interfaces
+        is.desktop.api = ['not'];
 
-    // is current operating system linux?
-    is.linux = function() {
-        return /linux/i.test(appVersion);
-    };
-    // linux method does not support 'all' and 'any' interfaces
-    is.linux.api = ['not'];
+        // is current operating system linux?
+        is.linux = function() {
+            return /linux/i.test(appVersion);
+        };
+        // linux method does not support 'all' and 'any' interfaces
+        is.linux.api = ['not'];
 
-    // is current operating system mac?
-    is.mac = function() {
-        return /mac/i.test(appVersion);
-    };
-    // mac method does not support 'all' and 'any' interfaces
-    is.mac.api = ['not'];
+        // is current operating system mac?
+        is.mac = function() {
+            return /mac/i.test(appVersion);
+        };
+        // mac method does not support 'all' and 'any' interfaces
+        is.mac.api = ['not'];
 
-    // is current operating system windows?
-    is.windows = function() {
-        return /win/i.test(appVersion);
-    };
-    // windows method does not support 'all' and 'any' interfaces
-    is.windows.api = ['not'];
+        // is current operating system windows?
+        is.windows = function() {
+            return /win/i.test(appVersion);
+        };
+        // windows method does not support 'all' and 'any' interfaces
+        is.windows.api = ['not'];
 
-    // is current state online?
-    is.online = function() {
-        return navigator.online;
-    };
-    // online method does not support 'all' and 'any' interfaces
-    is.online.api = ['not'];
+        // is current state online?
+        is.online = function() {
+            return navigator.online;
+        };
+        // online method does not support 'all' and 'any' interfaces
+        is.online.api = ['not'];
 
-    // is current state offline?
-    is.offline = not(is.online);
-    // offline method does not support 'all' and 'any' interfaces
-    is.offline.api = ['not'];
+        // is current state offline?
+        is.offline = not(is.online);
+        // offline method does not support 'all' and 'any' interfaces
+        is.offline.api = ['not'];
+    }
 
     // Object checks
     /* -------------------------------------------------------------------------- */
