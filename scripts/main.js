d198087426b9fba4030cfd53dbd0d4d698431960
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-01-28 02:59:36 +0200

    Add side navigation.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
 $(document).ready(function() {
     $('[data-typer-targets]').typer();
     $.typer.options.highlightSpeed    = 20;
-    $.typer.options.typeSpeed         = 40,
-    $.typer.options.clearDelay        = 1000,
-    $.typer.options.typeDelay         = 200,
-    $.typer.options.clearOnHighlight  = true,
-    $.typer.options.typerInterval     = 4000
+    $.typer.options.typeSpeed         = 40;
+    $.typer.options.clearDelay        = 1000;
+    $.typer.options.typeDelay         = 200;
+    $.typer.options.clearOnHighlight  = true;
+    $.typer.options.typerInterval     = 4000;
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 1410) {
+            $(".side-navigation").addClass("fixed");
+        } else {
+            $(".side-navigation").removeClass("fixed");
+        }
+    });
 });
