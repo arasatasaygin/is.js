92d51fb5c47cce9584b6e8217aa8dfd49fbe2bf6
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-02-12 17:32:52 +0200

    Update version. [opera check fix]

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-// is.js 0.5.1
+// is.js 0.5.2
 // Author: Aras Atasaygin
 
 // AMD with global, Node, or global
@@ -30,7 +30,7 @@
 
     // define 'is' object and current version
     is = {};
-    is.VERSION = '0.5.1';
+    is.VERSION = '0.5.2';
 
     // define interfaces
     is.not = {};
