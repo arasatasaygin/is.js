e6c56cc032a91d59c183e9f78d26d983fa472a08
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-02-24 11:46:28 +0200

    Update version. [0.7.0]

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-// is.js 0.6.0
+// is.js 0.7.0
 // Author: Aras Atasaygin
 
 // AMD with global, Node, or global
@@ -30,7 +30,7 @@
 
     // define 'is' object and current version
     is = {};
-    is.VERSION = '0.6.0';
+    is.VERSION = '0.7.0';
 
     // define interfaces
     is.not = {};
