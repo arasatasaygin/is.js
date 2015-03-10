8df50cd001b9e393a138f7c89ef7d794b29cf24e
aras.atasaygin <aras.atasaygin@publik.com.tr>
2015-03-10 22:17:04 +0200

    Update version. [blackberry check fix]

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-// is.js 0.7.1
+// is.js 0.7.2
 // Author: Aras Atasaygin
 
 // AMD with global, Node, or global
@@ -30,7 +30,7 @@
 
     // define 'is' object and current version
     is = {};
-    is.VERSION = '0.7.1';
+    is.VERSION = '0.7.2';
 
     // define interfaces
     is.not = {};
