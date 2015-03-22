3ce77dd8b5558c96870af34aec9ee0c90883b333
Ray Clanan <rclanan@utopianconcept.com>
2015-03-22 04:10:26 -0400

    Add check to is.sorted for descending-ordered arrays.

--- test/test.js
+++ test/test.js
@@ -3114,8 +3114,16 @@ describe("array checks", function() {
             var arr = [1, 2, 3, 4, 5];
             expect(is.sorted(arr)).to.be.true;
         });
+        it("should return true if given array is sorted", function() {
+            var arr = [5, 4, 3, 2, 1];
+            expect(is.sorted(arr)).to.be.true;
+        });
         it("should return false if given array is not sorted", function() {
             var arr = [1, 2, 3, 5, 4];
+            expect(is.sorted(arr)).to.be.false;
+        });
+        it("should return false if given array is not sorted", function() {
+            var arr = [5, 4, 3, 1, 2];
             expect(is.sorted(arr)).to.be.false;
         });
     });
@@ -3124,8 +3132,16 @@ describe("array checks", function() {
             var arr = [1, 2, 3, 4, 5];
             expect(is.not.sorted(arr)).to.be.false;
         });
+        it("should return false if given array is sorted", function() {
+            var arr = [5, 4, 3, 2, 1];
+            expect(is.not.sorted(arr)).to.be.false;
+        });
         it("should return true if given array is not sorted", function() {
             var arr = [1, 2, 3, 5, 4];
+            expect(is.not.sorted(arr)).to.be.true;
+        });
+        it("should return true if given array is not sorted", function() {
+            var arr = [5, 4, 3, 1, 2];
             expect(is.not.sorted(arr)).to.be.true;
         });
     });
@@ -3136,9 +3152,21 @@ describe("array checks", function() {
             expect(is.all.sorted(arr1, arr2)).to.be.true;
             expect(is.all.sorted([arr1, arr2])).to.be.true;
         });
+        it("should return true if all given arrays are sorted", function() {
+            var arr1 = [9, 8, 7, 6, 5];
+            var arr2 = [5, 4, 3, 2, 1];
+            expect(is.all.sorted(arr1, arr2)).to.be.true;
+            expect(is.all.sorted([arr1, arr2])).to.be.true;
+        });
         it("should return false if any given array is not sorted", function() {
             var arr1 = [1, 2, 3, 4, 5];
             var arr2 = [5, 6, 7, 9, 8];
+            expect(is.all.sorted(arr1, arr2)).to.be.false;
+            expect(is.all.sorted([arr1, arr2])).to.be.false;
+        });
+        it("should return false if any given array is not sorted", function() {
+          var arr1 = [9, 8, 7, 6, 5];
+          var arr2 = [5, 4, 3, 1, 2];
             expect(is.all.sorted(arr1, arr2)).to.be.false;
             expect(is.all.sorted([arr1, arr2])).to.be.false;
         });
@@ -3146,6 +3174,12 @@ describe("array checks", function() {
     describe("is.any.sorted", function() {
         it("should return true if any given array is sorted", function() {
             var arr1 = [1, 2, 3, 4, 5];
+            var arr2 = [10, 6, 7, 8, 9];
+            expect(is.any.sorted(arr1, arr2)).to.be.true;
+            expect(is.any.sorted([arr1, arr2])).to.be.true;
+        });
+        it("should return true if any given array is sorted", function() {
+            var arr1 = [5, 4, 3, 2, 1];
             var arr2 = [10, 6, 7, 8, 9];
             expect(is.any.sorted(arr1, arr2)).to.be.true;
             expect(is.any.sorted([arr1, arr2])).to.be.true;
