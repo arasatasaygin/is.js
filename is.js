49bf4c05b060460a3649618a901c209b563ed98b
arasatasaygin <arasatasaygin@gmail.com>
2016-01-03 12:58:16 +0200

    Update version to 0.7.4
    - This version fix dependency management issues

--- is.js
+++ is.js
@@ -1,4 +1,4 @@
-// is.js 0.7.4
+// is.js 0.7.5
 // Author: Aras Atasaygin
 
 // AMD with global, Node, or global
@@ -30,7 +30,7 @@
 
     // define 'is' object and current version
     var is = {};
-    is.VERSION = '0.7.4';
+    is.VERSION = '0.7.5';
 
     // define interfaces
     is.not = {};
