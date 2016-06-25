18ebd7dccc0d778941b0c1903a32a834e8594396
Andrey Prokopyuk <andrey.prokopyuk@gmail.com>
2016-06-25 16:59:03 +0300

    Add ESLint and its infrastructure (#205)
    - Add ESLint
    - Add and setup pre-commit package to validate diffs before commit
    - Add pragmas to code and fix issue with semilicon

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    env: {
+        browser: true,
+        node: true,
+        amd: true
+    },
+    globals: {
+        DocumentTouch: false
+    }
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    env: {
+        mocha: true
+    },
+    globals: {
+        is: false,
+        chai: false
+    }
+};
