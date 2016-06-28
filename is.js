90b74746cb1178b1a23ff941aab341fca87e89d3
John-David Dalton <john.david.dalton@gmail.com>
2016-06-28 12:29:49 -0700

    Fix typo in comment.

--- is.js
+++ is.js
@@ -346,7 +346,7 @@
         ipv6: /^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i
     };
 
-    // create regexp checks methods from 'regexp' object
+    // create regexp checks methods from 'regexes' object
     for (var regexp in regexes) {
         if (regexes.hasOwnProperty(regexp)) {
             regexpCheck(regexp, regexes);
