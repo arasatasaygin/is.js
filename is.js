a0b7eca2d1475ba753cc53becd8d239fee43b8b9
Tim Gates <tim.gates@iress.com>
2020-09-06 10:01:16 +1000

    docs: Fix simple typo, enviroments -> environments
    There is a small typo in is.js.
    
    Should read `environments` rather than `enviroments`.

--- is.js
+++ is.js
@@ -15,7 +15,7 @@
         });
     } else if (typeof exports === 'object') {
         // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like enviroments that support module.exports,
+        // only CommonJS-like environments that support module.exports,
         // like Node.
         module.exports = factory();
     } else {
